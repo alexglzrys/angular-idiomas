40e6dd88cebf70826a6850a1a08cddcc8a42e097
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-18 12:49:18 -0600

    instalar y configurar - NGX-Translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// Biblioteca de Traducción
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Factory para el cargador de idiomas, los cuales se consiguen haciendo peticiones http a sus archivos correspondientes
+export function cargarArchivosIdiomaJson(http: HttpClient) {
+  // Especificar ruta y extensión de archvo
+  return new TranslateHttpLoader(http, '/src/assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +21,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({                   // forRoot solo debe usarse en el módulo principal de la aplicación, forChild si se desea usar la traducción en módulos secundarios
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,               // Proveer el cargador de traducciones
+        useFactory: (cargarArchivosIdiomaJson),   // La fabrica encargada de propocionar el archivo de idioma
+        deps: [HttpClient]                      // La carga de archivos de idioma, depende del servicio de http
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
